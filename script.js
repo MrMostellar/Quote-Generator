4901e7845066c961159ee80c92ac267680db5f96
Andrew M <mr.mostellar@gmail.com>
2023-02-23 15:15:20 -0700

    added more functionality to the buttons and set up twitter button.

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const sourceWebsite = window.location.href;
+
 let apiQuotes = [];
 function newQuote(){
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
-}
+    //grab the author and text objects from the api result
+    if(!quote.author){
+        authorText.textContent = "Unknown";
+    } else{
+        authorText.textContent = quote.author;
+    }
+    if (quote.text.length > 120){
+        quoteText.classList.add("long-quote");
+    } else{
+        quoteText.classList.remove("long-quote");
+    }
+    quoteText.textContent = quote.text;
+    }
 
 // Get quotes from API, using asynchronous javascript
 async function getQuotes(){
@@ -19,5 +37,20 @@ async function getQuotes(){
     }
 }
 
+function tweetQuote(){
+    // Make a template string value for your tweet. Template strings allow you to pass variables into the string.
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent} Generated from Quote Generator: ${sourceWebsite.textContent}`;
+    // open the above url with template string in a new tab.
+    window.open(twitterUrl, '_blank');
+}
+
 // on load run this to get started.
 getQuotes();
+//on button press, run newQuote again.
+newQuoteBtn.addEventListener("click", () => {
+    newQuote();
+});
+
+twitterBtn.addEventListener("click", () => {
+    tweetQuote();
+})

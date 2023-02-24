9ce3ae9ba32f39ff21504dc18118f8fdecbaf041
Andrew M <mr.mostellar@gmail.com>
2023-02-23 18:00:30 -0700

    completed project by adding a loader, customizing it, making functions for it and implementing them.

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 const sourceWebsite = window.location.href;
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
+
+// show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//hide loading
+function complete(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 function newQuote(){
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     //grab the author and text objects from the api result
     if(!quote.author){
@@ -19,13 +34,16 @@ function newQuote(){
     } else{
         quoteText.classList.remove("long-quote");
     }
+    //set quote, hide loader
     quoteText.textContent = quote.text;
+    complete();
     }
 
 // Get quotes from API, using asynchronous javascript
 async function getQuotes(){
     const apiUrl = "https://type.fit/api/quotes";
     try {
+        loading();
         // this constant wont be populated until the data is fetched from the api
         const response = await fetch(apiUrl);
         //turn the response into a json for easy management
@@ -39,18 +57,15 @@ async function getQuotes(){
 
 function tweetQuote(){
     // Make a template string value for your tweet. Template strings allow you to pass variables into the string.
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}    -Quote Generator: ${sourceWebsite}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}   - ${authorText.textContent}    Quote Generator: ${sourceWebsite}`;
     // open the above url with template string in a new tab.
     window.open(twitterUrl, '_blank');
 }
 
+//on button press, run newQuote again.
+newQuoteBtn.addEventListener("click", newQuote);
+
+twitterBtn.addEventListener("click", tweetQuote);
+
 // on load run this to get started.
 getQuotes();
-//on button press, run newQuote again.
-newQuoteBtn.addEventListener("click", () => {
-    newQuote();
-});
-
-twitterBtn.addEventListener("click", () => {
-    tweetQuote();
-})
